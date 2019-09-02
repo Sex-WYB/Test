c326f7178fb9f73767fd99a62cf5c18ef914a421
Type-WYB <49379064+Type-WYB@users.noreply.github.com>
2019-09-02 11:21:07 +0800

    commit

--- src/Main/Mian.java
+++ src/Main/Mian.java
@@ -4,4 +4,7 @@ public class Mian {
     public static void main(String[] args) {
         System.out.print("sdfsdfdte");
     }
+    public void Branch(){
+        System.out.print("Branch");
+    }
 }
