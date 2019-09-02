a9ac41975fbbb649bd9f56fa9ee6f37222074a97
Type-WYB <49379064+Type-WYB@users.noreply.github.com>
2019-09-02 11:10:52 +0800

    commit 8-

--- src/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-import sun.awt.geom.AreaOp;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("sdfdsfsd++++++++testfristgit");
-    }
-}
